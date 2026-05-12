08dca841b9fa10dc9eb5100fa80df68b90f660bd
Gadkigen <kigengad@gmail.com>
2026-05-12 18:37:55 +0300

    "delete ai assisted projects"

--- README.md
+++
@@ -1,7 +0,0 @@
-#python practice
-This is my python learning journey.
-#projects
-   . calculator.py
-   . challenge.py
-   . contacts.json
-   . contacts.py
--- lads.py
+++
@@ -1,7 +0,0 @@
-import json 
-data = json.loads('{"name": "Gad"}')
-text = json.dumps(data, indent=2)
-print(text)
-with open("data.json") as f:
-    data = json.load(f)
-    print(data)
